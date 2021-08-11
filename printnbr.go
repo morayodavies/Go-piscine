3b65c303b7d8fa24555bff349e7c2cdb9f49bdf3
morayod <morayodav@gmail.com>
2021-08-11 18:32:14 +0100

    print nbr

---
+++ printnbr.go
@@ -0,0 +1,30 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+
+	res := 0
+	remainder := 0
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n * -1
+	}
+	if n >= 100 {
+		z01.PrintRune('1')
+		n -= 100
+		remainder = n % 10
+	}
+	if n >= 10 {
+		res = (n/10 + 48)
+		remainder = n % 10
+		z01.PrintRune(rune(res))
+		n = remainder
+	}
+	if n >= 0 {
+		remainder += 48
+		z01.PrintRune(rune(remainder))
+	}
+
+}
