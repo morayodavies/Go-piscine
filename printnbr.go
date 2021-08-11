d4e3de859bec9f0a7922b584dca4f0ca7ce74521
morayod <morayodav@gmail.com>
2021-08-11 19:39:13 +0100

    print nbr

--- printnbr.go
+++ printnbr.go
@@ -9,16 +9,15 @@ func PrintNbr(n int) {
 		z01.PrintRune('-')
 		n = n * -1
 	}
-	for divisor > 1 {
+	for divisor >= 1 {
 		if n >= divisor {
 			res = (n / divisor) + 48
 			z01.PrintRune(rune(res))
 			n = n - ((n / divisor) * divisor)
-			if n < divisor/10 && n > 0 {
-				z01.PrintRune('0')
-			}
+		}
+		if n%divisor == 0 && divisor > 1 {
+			z01.PrintRune('0')
 		}
 		divisor /= 10
 	}
-	z01.PrintRune(rune(n + 48))
 }
