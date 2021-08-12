02139e74b5a7624a734617888398805f0ceb9627
morayod <morayodav@gmail.com>
2021-08-12 19:13:13 +0100

    go ii exercise 9

--- basicatoi.go
+++ basicatoi.go
@@ -6,14 +6,16 @@ func BasicAtoi(s string) int {
 	for index := range word {
 		wordlength = index
 	}
-	n := int(word[wordlength]) - 48
 	i := 10
+	n := 0
 	for index := range word {
 		if index > 0 {
 			lastletter := word[wordlength-index]
 			e := int(lastletter) - 48
 			n = n + (e * i)
 			i *= 10
+		} else {
+			n = int(word[wordlength]) - 48
 		}
 	}
 	return n
